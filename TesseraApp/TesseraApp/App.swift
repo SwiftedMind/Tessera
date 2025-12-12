14f8b03ee65b7e252a9926c88b167ad0a4146205
Dennis Müller <d3mueller@me.com>
2025-12-12 18:41:13 +0100

    feat: add basic about window

--- TesseraApp/TesseraApp/App.swift
+++ TesseraApp/TesseraApp/App.swift
@@ -4,11 +4,33 @@ import SwiftUI
 
 @main
 struct TesseraApp: App {
+  @Environment(\.openWindow) private var openWindow
+  
   var body: some Scene {
     DocumentGroup(newDocument: TesseraDocument()) { file in
       Root(document: file.$document)
     }
     .defaultPosition(.center)
     .defaultSize(width: 1000, height: 800)
+    .commands {
+      CommandGroup(replacing: CommandGroupPlacement.appInfo) {
+        Button(action: {
+          openWindow(id: "about")
+        }, label: {
+          Text("About My App")
+        })
+      }
+    }
+    Window("About My App", id: "about") {
+      AboutView()
+    }
+    .defaultSize(width: 250, height: 300)
   }
 }
+
+struct AboutView: View {
+  var body: some View {
+    Text("HELLO, World")
+  }
+}
+
