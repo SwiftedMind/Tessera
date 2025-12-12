bbcc10919a58d99fb46a76035c4d36a07c2e7b54
Dennis Müller <d3mueller@me.com>
2025-12-12 20:58:38 +0100

    feat: add acknowledgements

--- TesseraApp/TesseraApp/App.swift
+++ TesseraApp/TesseraApp/App.swift
@@ -1,11 +1,12 @@
 // By Dennis Müller
 
+import SwiftPackageListUI
 import SwiftUI
 
 @main
 struct TesseraApp: App {
   @Environment(\.openWindow) private var openWindow
-  
+
   var body: some Scene {
     DocumentGroup(newDocument: TesseraDocument()) { file in
       Root(document: file.$document)
@@ -25,12 +26,48 @@ struct TesseraApp: App {
       AboutView()
     }
     .defaultSize(width: 250, height: 300)
+    Window("Acknowledgements", id: "acknowledgements") {
+      NavigationStack {
+        AcknowledgementsView()
+      }
+    }
+    .defaultSize(width: 520, height: 640)
   }
 }
 
 struct AboutView: View {
+  @Environment(\.openWindow) private var openWindow
+
   var body: some View {
-    Text("HELLO, World")
+    NavigationStack {
+      VStack( spacing: .medium) {
+        Image(.tesseraAppIcon)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 100, height: 100)
+          .clipShape(.rect(cornerRadius: 15))
+        Text("Tessera")
+          .font(.title)
+          .fontWeight(.semibold)
+        
+        Spacer()
+        
+        Divider()
+        
+        Button("Acknowledgements…") {
+          openWindow(id: "acknowledgements")
+        }
+      }
+      .padding()
+      .frame(minWidth: 250)
+    }
   }
 }
 
+struct AcknowledgementsView: View {
+  var body: some View {
+    NavigationStack {
+      AcknowledgmentsList()
+    }
+  }
+}
